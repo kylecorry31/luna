fb2045fe07f791a06b52225d7134a3eda7e67c32
Kyle Corry <kylecorry31@gmail.com>
2024-03-15 15:56:02 -0400

    Add more tests for hooks

--- src/test/kotlin/com/kylecorry/luna/cache/HooksTest.kt
+++ src/test/kotlin/com/kylecorry/luna/cache/HooksTest.kt
@@ -81,4 +81,111 @@ class HooksTest {
         assertEquals(1, count2)
     }
 
+    @Test
+    fun resetEffects() {
+        val hooks = Hooks()
+        var count = 0
+        var count2 = 0
+
+        hooks.effect("test", 1, "Test") {
+            count++
+        }
+
+        hooks.resetEffects()
+
+        hooks.effect("test", 1, "Test") {
+            count++
+        }
+
+        hooks.effect("test2", 1, "Test") {
+            count2++
+        }
+
+        assertEquals(2, count)
+        assertEquals(1, count2)
+
+        hooks.resetEffects(keys = listOf("test"))
+
+        hooks.effect("test", 1, "Test") {
+            count++
+        }
+
+        hooks.effect("test2", 1, "Test") {
+            count2++
+        }
+
+        assertEquals(3, count)
+        assertEquals(1, count2)
+
+        hooks.resetEffects(except = listOf("test"))
+
+        hooks.effect("test", 1, "Test") {
+            count++
+        }
+
+        hooks.effect("test2", 1, "Test") {
+            count2++
+        }
+
+        assertEquals(3, count)
+        assertEquals(2, count2)
+    }
+
+    @Test
+    fun resetMemos() {
+        val hooks = Hooks()
+        var count = 0
+        var count2 = 0
+
+        hooks.memo("test", 1, "Test") {
+            count++
+            "Hello"
+        }
+
+        hooks.resetMemos()
+
+        hooks.memo("test", 1, "Test") {
+            count++
+            "Hello"
+        }
+
+        hooks.memo("test2", 1, "Test") {
+            count2++
+            "Hello"
+        }
+
+        assertEquals(2, count)
+        assertEquals(1, count2)
+
+        hooks.resetMemos(keys = listOf("test"))
+
+        hooks.memo("test", 1, "Test") {
+            count++
+            "Hello"
+        }
+
+        hooks.memo("test2", 1, "Test") {
+            count2++
+            "Hello"
+        }
+
+        assertEquals(3, count)
+        assertEquals(1, count2)
+
+        hooks.resetMemos(except = listOf("test"))
+
+        hooks.memo("test", 1, "Test") {
+            count++
+            "Hello"
+        }
+
+        hooks.memo("test2", 1, "Test") {
+            count2++
+            "Hello"
+        }
+
+        assertEquals(3, count)
+        assertEquals(2, count2)
+    }
+
 }
